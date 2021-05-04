a088a20a06976300efb7fc71d54b98429f1214d7
kelvin9314 <kelvin9314@gmail.com>
2021-05-04 11:17:15 +0800

    Typography component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,32 @@
 import React from 'react'
+// import {Typography} from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+      Create a new notes
+      </Typography>
+      {/* <Typography
+        variant="h1"
+        color="primary"
+        align="center"
+      >
+      Create a new notes
+      </Typography> */}
+      {/* <Typography
+        noWrap
+        color="secondary"
+      >
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
+      </Typography> */}
     </div>
   )
 }
