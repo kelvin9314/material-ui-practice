0bcb0f8c77ccfd692a701946619d2d6a39b1a93a
kelvin9314 <kelvin9314@gmail.com>
2021-05-05 11:10:58 +0800

    button Component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,13 @@
 import React from 'react'
 // import {Typography} from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
-
+import Button from '@material-ui/core/Button';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Container from '@material-ui/core/Container';
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
@@ -27,6 +29,25 @@ export default function Create() {
       >
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
       </Typography> */}
-    </div>
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        // disableElevation
+      >
+        SUbmit
+      </Button>
+
+      {/* <Button type="submit" color="primary">Submit</Button>
+      <Button type="submit" variant="outlined" color="secondary">Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
