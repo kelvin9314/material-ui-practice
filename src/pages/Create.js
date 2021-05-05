82536b54652722ad3585d50d2fd305fa0de5e90a
kelvin9314 <kelvin9314@gmail.com>
2021-05-05 16:36:25 +0800

    Text Fields compoent

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import {Typography} from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
 // import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
 // import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 // import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
@@ -11,26 +12,43 @@ import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  // bth: {
-  //   fontSize: 40,
-  //   backgroundColor: 'violet',
-  //   '&:hover': {
-  //     backgroundColor: 'blue',
-  //   }
-  // },
-  // title: {
-  //   textDecoration: 'underline',
-  //   marginBottom: 20
-  // }
+  filed: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+
 })
 
 export default function Create() {
-  // const classes = useStyles()
+  const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const resetErrors = () => {
+    setTitleError(false)
+    setDetailsError(false)
+  }
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+    resetErrors()
+
+    if(title === '') setTitleError(true)
+    if(details === '') setDetailsError(true)
+
+    if(title && details) {
+      console.log(title,  details)
+
+    }
+
+  }
 
   return (
     <Container>
       <Typography
-        // className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -52,18 +70,42 @@ export default function Create() {
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
       </Typography> */}
 
-      <Button
-        // className={classes.bth}
-        onClick={() => console.log('you clicked me')}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        // disableElevation
-        // startIcon={<SendIcon />}
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        SUbmit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handlerSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.filed}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.filed}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          // disableElevation
+          // startIcon={<SendIcon />}
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          SUbmit
+        </Button>
+
+      </form>
+
 
       {/* <Button type="submit" color="primary">Submit</Button>
       <Button type="submit" variant="outlined" color="secondary">Submit</Button>
