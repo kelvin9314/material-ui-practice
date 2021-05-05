fe862b00b17d91ffe495897f4af2a842425a4d8f
kelvin9314 <kelvin9314@gmail.com>
2021-05-05 11:23:07 +0800

    install icons package and use it within a button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,6 +4,9 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
 // import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Container from '@material-ui/core/Container';
+// import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -36,6 +39,8 @@ export default function Create() {
         color="secondary"
         variant="contained"
         // disableElevation
+        // startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         SUbmit
       </Button>
@@ -48,6 +53,15 @@ export default function Create() {
         <Button>Two</Button>
         <Button>Three</Button>
       </ButtonGroup> */}
+
+      {/* icons */}
+      {/* <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
+      <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
+      <AcUnitOutlinedIcon color="action" fontSize="small"/>
+      <AcUnitOutlinedIcon color="error" fontSize="small"/>
+      <AcUnitOutlinedIcon color="disabled" fontSize="small"/> */}
     </Container>
   )
 }
