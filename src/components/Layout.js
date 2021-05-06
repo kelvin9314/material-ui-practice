87677676c22417d788884555e842f1ace0c1a4db
kelvin9314 <kelvin9314@gmail.com>
2021-05-06 13:56:20 +0800

    Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,23 @@
 import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import { Drawer, Typography } from '@material-ui/core';
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
   page: {
     backgroundColor: '#f9f9f9',
     width: '100%'
+  },
+  drawer: {
+    width: drawerWidth,
+
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: 'flex'
   }
 })
 
@@ -12,9 +25,19 @@ export default function Layout({ children }) {
   const classes = useStyles()
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar ? */}
       {/* side drawer ? */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5"> Kelvin Notes </Typography>
+        </div>
+      </Drawer>
       <div className={classes.page}>
         { children }
       </div>
