769db967e0d40aae08ffcc7b49e9221409761d3a
kelvin9314 <kelvin9314@gmail.com>
2021-05-18 10:52:52 +0800

    Lists & List Items

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,12 @@
 import { makeStyles } from '@material-ui/core'
 import React from 'react'
 import { Drawer, Typography } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
+import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240
 
@@ -18,11 +24,29 @@ const useStyles = makeStyles({
   },
   root: {
     display: 'flex'
+  },
+  active: {
+    background: '#f4f4f4'
   }
 })
 
 export default function Layout({ children }) {
+  const history = useHistory()
   const classes = useStyles()
+  const location = useLocation()
+
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color="secondary" />,
+      path: '/',
+    },
+    {
+      text: 'Create Note',
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: '/create',
+    },
+  ]
 
   return (
     <div className={classes.root}>
@@ -37,6 +61,22 @@ export default function Layout({ children }) {
         <div>
           <Typography variant="h5"> Kelvin Notes </Typography>
         </div>
+
+        {/* list / links */}
+        <List>
+          { menuItems.map( item => (
+            <ListItem
+              key={item.text}
+              button
+              onClick={() => history.push(item.path)}
+              className={ location.pathname === item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text}/>
+            </ListItem>
+          ))}
+        </List>
+
       </Drawer>
       <div className={classes.page}>
         { children }
